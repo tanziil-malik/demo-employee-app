7926b9793b8ae67b223aeb52ea93304fab2609f5
tanziil-malik <tanziilmalik107@gmail.com>
2026-04-15 01:21:16 +0500

    terraform updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,23 @@
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get subnets inside default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# EC2 Instance
 resource "aws_instance" "devops_server" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
   instance_type = "t2.micro"
+
+  # 🔥 FIX: explicitly assign subnet
+  subnet_id = data.aws_subnets.default.ids[0]
 
   tags = {
     Name = "devops-ec2"
